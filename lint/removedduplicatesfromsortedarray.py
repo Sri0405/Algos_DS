bd9bf26d0a7bb4e2cfdb69ed11abe9468ce8b776
sridhar <sridharb@buffalo.edu>
2015-10-07 17:41:29 -0400

    Remove Duplicates from sorted array

---
+++ lint/removedduplicatesfromsortedarray.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param A: a list of integers
+    @return an integer
+    """
+    def removeDuplicates(self, A):
+        # write your code here
+        if len(A)<2:
+            return A
+
+        start =0
+        for i in range(len(A)-1):
+             if A[i]!=A[start]:
+                start+=1
+                if i!= start:
+                    A[start]=A[i]
+        return start+1
+
+print Solution().removeDuplicates([-14,-14,-13,-13,-13,-13,-13,-13,-13,-12,-12,-12,-12,-11,-10,-9,-9,-9,-8,-7,-5,-5,-5,-5,-4,-3,-3,-2,-2,-2,-2,-1,-1,-1,-1,-1,0,1,1,1,1,2,2,2,3,3,3,4,4,4,4,5,5,5,6,6,6,6,7,8,8,8,9,9,9,10,10,10,11,11,11,12,12,12,13,14,14,14,14,15,16,16,16,18,18,18,19,19,19,19,20,20,20,21,21,21,21,21,21,22,22,22,22,22,23,23,24,25,25])    
